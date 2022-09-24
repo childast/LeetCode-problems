592abb7b310c492bbda92a09e14956896ab70920
childast <33985338+childast@users.noreply.github.com>
2022-09-24 08:29:42 +0100

    Time: 63 ms (5.23%), Space: 9.6 MB (5.42%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        
+        vector<int> v;
+        while(x){
+            v.push_back(x%10);
+            x /= 10;
+        }
+        for(int i = 0; i < v.size() / 2; i++){
+            if(v[i] != v[v.size() - 1 - i])
+                return false;
+        }
+        return true;
+    }
+};
